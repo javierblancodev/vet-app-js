b26010a77437743a76af19afeb630bf208796d28
Javier Blanco <javierblancoit@gmail.com>
2022-08-08 07:21:52 +0100

    create functionality to edit changes

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
+let editando = false;
+
 // UI
 const formulario = document.querySelector("#nueva-cita");
 const contendorCitas = document.querySelector('#citas');
@@ -21,10 +23,43 @@ class Citas {
         ui.imprimirAlerta('La cita se ha guardado correctamente');
     }
 
+    editarCita(citaActualizada) {
+        ui.imprimirAlerta('Cambios guardados correctamente');
+        this.citas = this.citas.map( cita => cita.id === citaActualizada.id ? citaActualizada : cita );
+    }
+
     eliminarCita(id) {
         this.citas = this.citas.filter(cita => cita.id !== id);
         ui.imprimirAlerta('La cita ha sido eliminada correctamente');
         ui.imprimirCitas(this);
+    }
+
+    cargarEdicion(cita) {
+        const { mascota, propietario, telefono, fecha, hora, sintomas, id } = cita;
+
+        // Llena los inputs
+        mascotaInput.value = mascota;
+        propietarioInput.value = propietario;
+        telefonoInput.value = telefono;
+        fechaInput.value = fecha;
+        fechaInput.value = fecha;
+        horaInput.value = hora;
+        sintomasInput.value = sintomas;
+
+        // Llena el objeto de cita
+
+        citaObj.mascota = mascota;
+        citaObj.propietario = propietario;
+        citaObj.telefono = telefono;
+        citaObj.fecha = fecha;
+        citaObj.hora = hora;
+        citaObj.sintomas = sintomas;
+        citaObj.id = id;
+        
+        // Cambiar apariencia de boton
+        formulario.querySelector('button[type="submit"]').textContent = "Guardar Cambios"
+
+        editando = true;
     }
 }
 
@@ -89,8 +124,13 @@ class UI {
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
             btnEliminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>';
-
             btnEliminar.onclick = () => administrarCitas.eliminarCita(id);
+
+            // Agrega un boton para editar cita
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML= 'Editar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg>';
+            btnEditar.onclick = () => administrarCitas.cargarEdicion(cita);
 
             // Agrega elementos al contenedor de la cita
             divCita.appendChild(mascotaParrafo);
@@ -100,6 +140,7 @@ class UI {
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
             divCita.appendChild(btnEliminar);
+            divCita.appendChild(btnEditar);
 
             // Inserta el contenedor de la cita en el DOM
             contendorCitas.appendChild(divCita);
@@ -160,11 +201,18 @@ function nuevaCita(e) {
         return;
     }
 
-    // Generacion de id unico
-    citaObj.id = Date.now();
-    
-    // Adicion de nueva cita
-    administrarCitas.agregarCita({...citaObj}); // Pasamos una copia, no el objeto global. De lo contrario, se modificarian los datos en el listado cada vez que modificamos el objeto global
+    if(editando) {
+        formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita '
+        editando = false;
+        administrarCitas.editarCita({ ...citaObj })
+    } else {
+        // Generacion de id unico
+        citaObj.id = Date.now();
+
+        // Adicion de nueva cita
+        administrarCitas.agregarCita({...citaObj}); // Pasamos una copia, no el objeto global. De lo contrario, se modificarian los datos en el listado cada vez que modificamos el objeto global
+
+    }
 
     // Reinicio del formulario
     reiniciarObjeto();
